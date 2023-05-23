62b88fb1284cd4c59cd638d2f6cb400fc62a2f03
Leonardo DiAbreu <drakkull007@hotmail.com>
2023-05-23 10:25:26 -0300

    Starting a new project with JDBC

---
+++ src/engine/Program.java
@@ -0,0 +1,7 @@
+package engine;
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/entities/DbIntegrityException.java
@@ -0,0 +1,9 @@
+package entities;
+
+public class DbIntegrityException extends RuntimeException{
+
+    public DbIntegrityException (String msg){
+        super (msg);
+    }
+
+}
