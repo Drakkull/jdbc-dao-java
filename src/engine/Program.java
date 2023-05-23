049a13a9a851469f645bf8b8d4ccec0352fed4d8
Leonardo DiAbreu <drakkull007@hotmail.com>
2023-05-23 10:34:30 -0300

    Added Department Class

--- src/engine/Program.java
+++ src/engine/Program.java
@@ -1,7 +1,12 @@
 package engine;
 
+import entities.Department;
+
 public class Program {
     public static void main(String[] args) {
+
+        Department dep = new Department(1,"Oeeeee");
+        System.out.println(dep);
 
     }
 }
