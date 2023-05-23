fce00e7fc2389f6e187cac1590c159971186c6cd
Leonardo DiAbreu <drakkull007@hotmail.com>
2023-05-23 10:38:19 -0300

    Testing the connection with DB

--- src/engine/Program.java
+++ src/engine/Program.java
@@ -1,9 +1,28 @@
 package engine;
 
+import entities.DB;
+import entities.DbException;
 import entities.Department;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Program {
     public static void main(String[] args) {
+
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try{
+            conn = DB.getConnection();
+
+        }
+        catch (DbException e){
+            e.printStackTrace();
+
+        }
+
 
         Department dep = new Department(1,"Oeeeee");
         System.out.println(dep);
