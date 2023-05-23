0e776d16f67f80cde83f4a20dc928e79f59adb68
Leonardo DiAbreu <drakkull007@hotmail.com>
2023-05-23 11:11:16 -0300

    Added Seller Class and Tests in the main program

--- src/engine/Program.java
+++ src/engine/Program.java
@@ -3,17 +3,20 @@ package engine;
 import entities.DB;
 import entities.DbException;
 import entities.Department;
+import entities.Seller;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Connection conn = null;
         PreparedStatement st = null;
-
+        System.out.println("--------------Testing DB connectivity----------");
         try{
             conn = DB.getConnection();
 
@@ -22,8 +25,12 @@ public class Program {
             e.printStackTrace();
 
         }
+        System.out.println("-----------Testing Seller---------------");
+        Seller sel = new Seller(1,"Pililiu","pil@gmail.com", new Date(),3.000, new Department(1, "xiii"));
+        System.out.println(sel);
 
 
+        System.out.println("-----------Testing Department------------");
         Department dep = new Department(1,"Oeeeee");
         System.out.println(dep);
 
